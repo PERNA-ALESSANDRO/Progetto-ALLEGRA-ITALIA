2412d5075166cc9217c19b3c8878d93164080026
ASFALTO-CALDO-FRESCO <124783727+ASFALTO-CALDO-FRESCO@users.noreply.github.com>
2023-02-08 12:08:33 +0100

    Update and rename README.md to source_arduino.c

---
+++ source_arduino.c
@@ -0,0 +1,58 @@
+// Progetto-ALLEGRA-ITALIA
+// C code
+//
+const int nPul = 6;
+const int pulsanti[nPul] = {2, 3, 4, 5, 6, 7};
+const int led[nPul] = {0, 1, 0, 1, 0, 1};
+const int LED_RED = 12;
+const int LED_GREEN = 13;
+const int BUZZER = 11;
+int statoPul;
+void rightTone();
+void wrongTone();
+void setup()
+{
+  for(int i = 0; i < nPul; i++){
+    pinMode(pulsanti[i], INPUT);
+  }
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+}
+void loop()
+{
+  for(int i = 0; i < nPul; i++){
+    if(digitalRead(pulsanti[i]) == LOW){
+      if(led[i] == 0){
+        digitalWrite(LED_RED, HIGH);
+        wrongTone();
+      } else {
+        digitalWrite(LED_GREEN, HIGH);
+        rightTone();
+      }
+      delay(1000);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      noTone(BUZZER);
+    }
+  }
+  delay(10);
+}
+void rightTone(){
+    tone(BUZZER, 783);
+    delay(250);
+    tone(BUZZER, 1046);
+    delay(250);
+    tone(BUZZER, 1576);
+    delay(500);
+    noTone(BUZZER);
+}
+void wrongTone(){
+  tone(BUZZER, 392);
+  delay(250);
+  tone(BUZZER, 261);
+  delay(250);
+  tone(BUZZER, 196);
+  delay(500);
+  noTone(BUZZER);
+}
