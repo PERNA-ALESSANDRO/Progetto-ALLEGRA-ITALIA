af70b46f490dd7d66804e0cb20382909563a4630
PERNA-ALESSANDRO <124783727+PERNA-ALESSANDRO@users.noreply.github.com>
2023-03-14 09:04:14 +0100

    Update source_arduino.c

--- source_arduino.c
+++ source_arduino.c
@@ -1,30 +1,20 @@
 // Progetto-ALLEGRA-ITALIA
 // C code
 //
-const int PORTA_MIN = 2; // Cambiare la prima porta 
-const int nPul = 6; // Cambiare numero dei pulsanti
+const int nPul = 6;
+const int pulsanti[nPul] = {2, 3, 4, 5, 6, 7};
 const int LED_RED = 12;
 const int LED_GREEN = 13;
 const int BUZZER = 11;
 
 int statoPul;
-int ans[nPul]{-1, -1, -1, -1, -1, -1};
-int pulsanti[nPul];
+int ans[nPul];
 
 void rightTone();
 void wrongTone();
 
-int a = Serial.available();
-
 void setup()
 {
-  Serial.begin(9600);
-  
-  // Inizializzazione dell'array delle porte di INPUT
-  for(int i = 0; i < nPul; i++){
-  	pulsanti[i] = i + PORTA_MIN;
-  }
-  
   // Settaggio di tutti i pin collegati ai pulsanti come INPUT
   for(int i = 0; i < nPul; i++){
     pinMode(pulsanti[i], INPUT);
@@ -36,12 +26,12 @@ void setup()
   pinMode(LED_GREEN, OUTPUT);
   
   // Settaggio bit di risposte
-  for(int i = 0; i < nPul; i++){
-    while(ans[i] == -1){
-  		ans[i] = insAns();
-    }
-
-  }
+  ans[0] = 0;	// Risposta 1
+  ans[1] = 1;	// Risposta 2
+  ans[2] = 0;	// Risposta 3
+  ans[3] = 1;	// Risposta 4
+  ans[4] = 0;	// Risposta 5
+  ans[5] = 1;	// Risposta 6
 }
 
 void loop()
@@ -49,7 +39,7 @@ void loop()
   for(int i = 0; i < nPul; i++){
     // Controllo pulsante premuto o no
     if(digitalRead(pulsanti[i]) == LOW){
-      if(ans[i] == '0'){
+      if(ans[i] == 0){
         digitalWrite(LED_RED, HIGH);
         wrongTone();
       } else {
@@ -86,14 +76,3 @@ void wrongTone(){
   delay(500);
   noTone(BUZZER);
 }
-
-int insAns(){
-  int inByte = -1;
-
-  if(Serial.available() > 0){
-    inByte = Serial.read();
-  }
-  
-  return inByte;
-}
-
